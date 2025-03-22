62bb908e68a5efdaae6750d390d7372fd3e7485e
Vanshika soni <106619869+vanshikasoni8@users.noreply.github.com>
2025-03-22 23:38:39 +0530

    [Vanshika Soni] Add: Brigdlabz Logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [RouterModule] // Add RouterModule here
 })
 export class AppComponent {
   title = 'HelloWorldApp';
+  logoPath: string  = 'assets/Logo.jpg';
 }
